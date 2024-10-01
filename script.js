c21061d8c3fdfd1a2af8f23bbd1065d09ee2fc79
Navneet_thakur <127828104+navneet451@users.noreply.github.com>
2024-10-01 14:07:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 //your code here
+(function () {
+  'use strict';
+  
+  // Fetch all the forms we want to apply custom Bootstrap validation styles to
+  var forms = document.querySelectorAll('.needs-validation');
+  
+  // Loop over the forms and prevent submission if validation fails
+  Array.prototype.slice.call(forms).forEach(function (form) {
+    form.addEventListener('submit', function (event) {
+      if (!form.checkValidity()) {
+        event.preventDefault();
+        event.stopPropagation();
+      }
+      
+      form.classList.add('was-validated');
+    }, false);
+  });
+})();
